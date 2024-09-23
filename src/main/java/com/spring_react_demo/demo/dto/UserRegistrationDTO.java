43faa393a53cae9eea34e9114849f3db1d71c79a
asif-kamal <asif.kamal1489@gmail.com>
2024-09-23 18:53:52 -0400

    Built the UserRegistrationDTO

--- src/main/java/com/spring_react_demo/demo/dto/UserRegistrationDTO.java
+++ src/main/java/com/spring_react_demo/demo/dto/UserRegistrationDTO.java
@@ -1,5 +1,18 @@
 package com.spring_react_demo.demo.dto;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRegistrationDTO {
     
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date dateOfBirth;
 }
