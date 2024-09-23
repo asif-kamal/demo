362b668b4053edbd092f8b10bda79c427b5ae853
asif-kamal <asif.kamal1489@gmail.com>
2024-09-23 18:51:28 -0400

    built security config file to authorize any requests made(for now)

---
+++ src/main/java/com/spring_react_demo/demo/config/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.spring_react_demo.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfiguration {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
