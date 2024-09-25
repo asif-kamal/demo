77ceff622231cbb724abfb9eefa909ea6032fed2
asif-kamal <asif.kamal1489@gmail.com>
2024-09-24 20:55:18 -0400

    added long for uid of exception from video

--- src/main/java/com/spring_react_demo/demo/exception/EmailAlreadyTakenException.java
+++ src/main/java/com/spring_react_demo/demo/exception/EmailAlreadyTakenException.java
@@ -2,6 +2,8 @@ package com.spring_react_demo.demo.exception;
 
 public class EmailAlreadyTakenException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
     public EmailAlreadyTakenException() {
         super("The email provided is already taken");
     }
