4a619c7b6bcbe4d66172c6a094564c5ca8eb8b51
asif-kamal <asif.kamal1489@gmail.com>
2024-09-25 21:04:22 -0400

    hard-coded no args constructor for User model, setting enabled property to false

--- src/main/java/com/spring_react_demo/demo/entity/User.java
+++ src/main/java/com/spring_react_demo/demo/entity/User.java
@@ -1,9 +1,9 @@
 package com.spring_react_demo.demo.entity;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 import java.sql.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -18,14 +18,11 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "users")
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
@@ -34,18 +31,17 @@ public class User {
     @Column(name = "user_id")
     private Long userId;
 
-    //@NotNull(message = "{demo.constraint.username.NotNull.message}")
-    @Column(name = "username", unique = true/*, nullable = false */)
+    // @NotNull(message = "{demo.constraint.username.NotNull.message}")
+    @Column(name = "username", unique = true/* , nullable = false */)
     private String username;
 
-    //@NotNull
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}$", 
-            message = "{demo.constraint.password.Pattern.message}")
-    // @JsonIgnore
-    @Column(name = "password"/*, nullable = false*/)
+    // @NotNull
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}$", message = "{demo.constraint.password.Pattern.message}")
+    //@JsonIgnore
+    @Column(name = "password"/* , nullable = false */)
     private String password;
 
-    @Column(name = "email", unique = true/* , nullable = false*/)
+    @Column(name = "email", unique = true/* , nullable = false */)
     private String email;
 
     @Column(name = "phone_number")
@@ -60,9 +56,20 @@ public class User {
     @Column(name = "dob")
     private Date dateOfBirth;
 
-    @ManyToMany(fetch=FetchType.EAGER)
-    @JoinTable(name = "user_role_junction", joinColumns = {@JoinColumn(name = "user_id")}, inverseJoinColumns = {@JoinColumn(name = "role_id")})
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role_junction", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
+            @JoinColumn(name = "role_id") })
     private Set<Role> authorities;
-    
-    
+
+    @Column
+    private Boolean enabled;
+
+    @Column(nullable = true)
+    @JsonIgnore
+    private Long verification;
+
+    public User() {
+        this.authorities = new HashSet<>();
+        this.enabled = false;
+    }
 }
