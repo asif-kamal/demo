e9cb4a7ae439ec33759c308be88d4275afa492aa
asif-kamal <asif.kamal1489@gmail.com>
2024-10-06 17:31:56 -0400

    replaced className with iconClass comp

--- frontend/src/features/register/components/RegistrationStep/RegistrationStep.tsx
+++ frontend/src/features/register/components/RegistrationStep/RegistrationStep.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { displayIcon } from '../../utils/RegisterStepUtils';
+import { displayIcon, iconClass } from '../../utils/RegisterStepUtils';
 import './RegistrationStep.css'
 
 interface RegisterStepProps {
     step: number;
+    changeStep():void
 }
 
-export const RegistrationStep:React.FC<RegisterStepProps> = ({step}) => {
+export const RegistrationStep:React.FC<RegisterStepProps> = ({step, changeStep}) => {
   return (
     <div className='reg-step-container'>
-        <div className='reg-step-btn'>
+        <div className={iconClass(step)} onClick={changeStep}>
             {displayIcon(step)}
       </div>
       <span className='reg-step-number'>Step {step} of 6</span>
