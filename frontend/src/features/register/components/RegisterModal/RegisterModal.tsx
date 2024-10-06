d55f0eefe0ad96ee4cabadddc06318b2450d0d4c
asif-kamal <asif.kamal1489@gmail.com>
2024-10-06 17:30:27 -0400

    built stepButtonClicked based on number of register modal

--- frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
+++ frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from '../../../../components/Modal/Modal'
 import './RegisterModal.css'
 import { RegistrationStep } from '../RegistrationStep/RegistrationStep'
 
 
 export const RegisterModal:React.FC = () => {
+
+  const [step, setStep] = useState<number>(4)
+
+  const stepButtonClicked = () => {
+    step === 1 || step === 4 || step >= 6 ? setStep(step) : setStep(step-1)
+  }
+
   return (
     <div className="register-modal">
       <Modal>
         <div className='register-container'>
-          <RegistrationStep step={1}/>
+          <RegistrationStep step={step} changeStep={stepButtonClicked}/>
         </div>
       </Modal>
     </div>
