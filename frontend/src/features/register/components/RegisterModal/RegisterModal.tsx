3ca67a5771583ee6ec3786253048b6929c166014
asif-kamal <asif.kamal1489@gmail.com>
2024-10-05 21:07:04 -0400

    hardcoded step number inside RegistrationStep comp

--- frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
+++ frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Modal } from '../../../../components/Modal/Modal'
 import './RegisterModal.css'
+import { RegistrationStep } from '../RegistrationStep/RegistrationStep'
+
 
 export const RegisterModal:React.FC = () => {
   return (
     <div className="register-modal">
       <Modal>
-        <h1>register modal</h1>
+        <div className='register-container'>
+          <RegistrationStep step={1}/>
+        </div>
       </Modal>
     </div>
   )
