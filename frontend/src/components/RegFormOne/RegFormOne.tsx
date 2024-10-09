9c15f5de32e7b5e7ed2c8cb572f9ec0a65ba1c50
asif-kamal <asif.kamal1489@gmail.com>
2024-10-08 20:56:47 -0400

    Created RegFormOne component for registration step 1

---
+++ frontend/src/components/RegFormOne/RegFormOne.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './RegFormOne.css'
+
+export const RegFormOne:React.FC = () => {
+  return (
+    <div className='reg-step-one-container'>
+      <div className='reg-step-one-content'>
+      User Input Area for Register 1
+      </div>
+    </div>
+  )
+}
+
+
