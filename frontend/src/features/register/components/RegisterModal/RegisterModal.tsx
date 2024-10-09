181356da6bf1dc9a9062616ec2decdcf0a033b21
asif-kamal <asif.kamal1489@gmail.com>
2024-10-08 20:58:28 -0400

    displaying content depending on step number in RegisterModal component

--- frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
+++ frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import { Modal } from '../../../../components/Modal/Modal'
 import './RegisterModal.css'
 import { RegistrationStep } from '../RegistrationStep/RegistrationStep'
+import { determineModalContent } from '../../utils/RegisterModalUtils'
 
 
 export const RegisterModal:React.FC = () => {
 
-  const [step, setStep] = useState<number>(4)
+  const [step, setStep] = useState<number>(1)
 
   const stepButtonClicked = () => {
     step === 1 || step === 4 || step >= 6 ? setStep(step) : setStep(step-1)
@@ -17,6 +18,9 @@ export const RegisterModal:React.FC = () => {
       <Modal>
         <div className='register-container'>
           <RegistrationStep step={step} changeStep={stepButtonClicked}/>
+          <div className='register-modal-content'>
+            {determineModalContent(step)}
+          </div>
         </div>
       </Modal>
     </div>
