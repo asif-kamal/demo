7e4c6365c78109d5375d0973f6a2ec3ed2476176
asif-kamal <asif.kamal1489@gmail.com>
2024-10-13 18:34:03 -0400

    renamed Home to Landing 2

---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Landing.css";
+import "../assets/global.css";
+import { Modal } from "../components/Modal/Modal";
+import { RegisterModal } from "../features/register/components/RegisterModal/RegisterModal";
+
+export const Landing: React.FC = () => {
+  return (
+    <div className="home-container bg-color">
+      <RegisterModal />
+    </div>
+  );
+};
