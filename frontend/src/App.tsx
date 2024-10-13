ccbf462574ce14b573c7d17192ac9d34931279c7
asif-kamal <asif.kamal1489@gmail.com>
2024-10-13 18:35:44 -0400

    added colors and font to match Twitter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import './assets/global.css';
-import { Home } from './pages/Home';
+import React from "react";
+import "./assets/global.css";
+import { Landing } from "./pages/Landing";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { Theme } from "./utils/GlobalInterfaces";
+
+const theme:Theme = {
+  colors: {
+    blue: "#1DA1F2",
+    black: "#14171a",
+    darkGray: "#657786",
+    lightGray: "#AAB8C2",
+    offWhite: "#E1E8ED",
+    white: "#F5F8FA",
+    error: "red",
+  },
+};
+
+const GlobalStyle = createGlobalStyle`
+* {
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: 500;
+}
+`;
 
 export const App = () => {
   return (
-   <Home />
-  )
-}
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Landing />
+    </ThemeProvider>
+  );
+};
